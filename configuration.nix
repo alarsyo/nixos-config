d1aeab79b1c8fdb413425a04b4fc938670e6d109
Antoine Martin <antoine97.martin@gmail.com>
2021-01-27 18:30:19 +0100

    fish: enable integrations and completions

--- configuration.nix
+++ configuration.nix
@@ -92,11 +92,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.fish.enable = true;
 
   # List services that you want to enable:
 
