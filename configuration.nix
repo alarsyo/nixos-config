3182ff0e895dafd1474617bf92f1b355ff0e39b7
Antoine Martin <antoine97.martin@gmail.com>
2021-01-27 18:26:59 +0100

    packages: add bat and stow

--- configuration.nix
+++ configuration.nix
@@ -80,9 +80,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bat
     fish
     git
     htop
+    stow
     tmux
     vim
     wget
