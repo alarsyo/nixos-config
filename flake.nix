3efc347bb80eb55dd3725f12056789a10becf4e6
Antoine Martin <antoine97.martin@gmail.com>
2021-02-06 03:09:11 +0100

    flake: init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Nixos configuration with flakes";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09-small";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.poseidon = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          ./configuration.nix
+        ];
+    };
+  };
+}
