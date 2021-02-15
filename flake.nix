1840ffb6140cf24a52598de226c7f88820ab4d79
Antoine Martin <antoine97.martin@gmail.com>
2021-02-15 17:23:25 +0100

    flake: add nixpkgs-unstable overlay

--- flake.nix
+++ flake.nix
@@ -2,16 +2,23 @@
   description = "Nixos configuration with flakes";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09-small";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.poseidon = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixpkgs-unstable }: {
+    nixosConfigurations.poseidon = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules =
         [
           ./poseidon.nix
+
           {
-            nixpkgs.overlays = import ./overlays;
+            nixpkgs.overlays = [
+              # packages accessible through pkgs.unstable.package
+              (final: prev: {
+                unstable = nixpkgs-unstable.legacyPackages.${system};
+              })
+            ] ++ (import ./overlays);
           }
         ];
     };
