cfb135e11842ff30f55f928f7fa6d167f24ecc02
Antoine Martin <antoine97.martin@gmail.com>
2021-02-19 21:46:19 +0100

    base: add main user to media group

--- base/users.nix
+++ base/users.nix
@@ -10,7 +10,10 @@ in
   users.users.alarsyo = {
     hashedPassword = secrets.shadow-hashed-password-alarsyo;
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "media"
+      "wheel" # Enable ‘sudo’ for the user.
+    ];
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3rrF3VSWI4n4cpguvlmLAaU3uftuX4AVV/39S/8GO9 alarsyo@thinkpad"
