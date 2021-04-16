9c294caf3d420cbb8bee4254e3ba1cb5241baa36
Antoine Martin <antoine@alarsyo.net>
2021-04-16 21:57:08 +0200

    boreal: enable wireguard

--- hosts/boreal/default.nix
+++ hosts/boreal/default.nix
@@ -37,6 +37,22 @@ in
 
   # List services that you want to enable:
   my.services = {
+    wireguard = {
+      enable = true;
+      iface = "wg";
+      port = 51820;
+
+      net = {
+        v4 = {
+          subnet = "10.0.0";
+          mask = 24;
+        };
+        v6 = {
+          subnet = "fd42:42:42";
+          mask = 64;
+        };
+      };
+    };
   };
 
   services = {
