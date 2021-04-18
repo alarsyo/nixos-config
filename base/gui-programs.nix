9a9087256fd93fa94ae9ad0f68daa000a90ef755
Antoine Martin <antoine@alarsyo.net>
2021-04-18 14:46:12 +0200

    base: add necessary emacs dependencies

--- base/gui-programs.nix
+++ base/gui-programs.nix
@@ -10,8 +10,15 @@
     pavucontrol
     slack
     spotify
+    sqlite # needed for org-roam
+  ];
+
+  fonts.fonts = with pkgs; [
+    input-fonts
+    emacs-all-the-icons-fonts
   ];
 
   networking.networkmanager.enable = true;
   programs.nm-applet.enable = true;
+  nixpkgs.config.input-fonts.acceptLicense = true;
 }
