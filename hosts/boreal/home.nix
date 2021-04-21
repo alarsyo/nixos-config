a25d5051d3d2a0d12ffdb6182ad1b96fe6b52799
Antoine Martin <antoine@alarsyo.net>
2021-04-21 04:07:38 +0200

    boreal: use solarized light theme

--- hosts/boreal/home.nix
+++ hosts/boreal/home.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   home-manager.users.alarsyo = {
     # Keyboard settings & i3 settings
     my.home.x.enable = true;
     my.home.emacs.enable = true;
     my.home.tmux.enable = true;
+    my.home.theme = config.home-manager.users.alarsyo.my.home.themes.solarizedLight;
   };
 }
