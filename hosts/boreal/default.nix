e002b8482ae455ed7320fbd1c067f9075d5f3830
Antoine Martin <antoine@alarsyo.net>
2021-06-10 02:07:58 +0200

    Revert "boreal: switch to linux latest"
    This reverts commit 78b7e86ee4c9766b55ec01311cb9631ee5035bc2.

--- hosts/boreal/default.nix
+++ hosts/boreal/default.nix
@@ -14,8 +14,6 @@ in
       ./home.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
