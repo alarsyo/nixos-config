786d884e3abff7191accb1efd57a6d95eafdceee
Antoine Martin <antoine@alarsyo.net>
2021-07-13 17:13:07 +0200

    services: monitoring: package grafana dashboards

--- services/monitoring.nix
+++ services/monitoring.nix
@@ -37,8 +37,13 @@ in {
 
         dashboards = [
           {
-            name = "Dashboards";
-            options.path = ./grafana-dashboards;
+            name = "Node Exporter";
+            options.path = pkgs.packages.grafana-dashboards.node-exporter;
+            disableDeletion = true;
+          }
+          {
+            name = "NGINX";
+            options.path = pkgs.packages.grafana-dashboards.nginx;
             disableDeletion = true;
           }
         ];
