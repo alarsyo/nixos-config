1d75c36f9e1e95253f4302647de0eabed4fae30d
Antoine Martin <antoine@alarsyo.net>
2021-07-29 12:20:30 +0200

    home: fish: enable by default

--- hosts/poseidon/home.nix
+++ hosts/poseidon/home.nix
@@ -1,7 +1,6 @@
 { config, ... }:
 {
   home-manager.users.alarsyo = {
-    my.home.fish.enable = true;
 
     my.theme = config.home-manager.users.alarsyo.my.themes.solarizedLight;
   };
--- hosts/zephyrus/home.nix
+++ hosts/zephyrus/home.nix
@@ -10,7 +10,6 @@
     my.home.x.cursor.enable = true;
     my.home.alacritty.enable = true;
     my.home.emacs.enable = true;
-    my.home.fish.enable = true;
 
     my.theme = config.home-manager.users.alarsyo.my.themes.solarizedLight;
 
