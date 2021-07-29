ba1e18165b2870780720fbaec822d9150f7729c9
Antoine Martin <antoine@alarsyo.net>
2021-07-29 12:19:31 +0200

    home: tmux: enable by default

--- hosts/boreal/home.nix
+++ hosts/boreal/home.nix
@@ -8,7 +8,6 @@
     my.home.x.cursor.enable = true;
     my.home.alacritty.enable = true;
     my.home.emacs.enable = true;
-    my.home.tmux.enable = true;
     my.home.fish.enable = true;
 
     my.theme = config.home-manager.users.alarsyo.my.themes.solarizedLight;
