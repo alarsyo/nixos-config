afd5444a88658bdf7fe1b24bb0d5eeccc6dc9357
Antoine Martin <antoine@alarsyo.net>
2021-07-29 12:22:46 +0200

    home: x: cursor: enable by default

--- hosts/boreal/home.nix
+++ hosts/boreal/home.nix
@@ -5,7 +5,6 @@
     my.home.x.enable = true;
     my.home.x.i3bar.temperature.chip = "k10temp-pci-*";
     my.home.x.i3bar.temperature.inputs =  [ "Tccd1" ];
-    my.home.x.cursor.enable = true;
     my.home.alacritty.enable = true;
     my.home.emacs.enable = true;
 
