abf1bd436a68dbeec76136a42aae71f7a98c7a09
Antoine Martin <antoine@alarsyo.net>
2021-11-05 16:06:10 +0100

    github: use new --json flag to get packages

--- .github/workflows/build-flake-packages.sh
+++ .github/workflows/build-flake-packages.sh
@@ -2,12 +2,8 @@
 
 set -xe
 
-PACKAGES=$(nix flake show \
-	| grep ': package' \
-	| sed 's/\x1b\[[0-9;]*m//g' \
-	| sed -E 's/^.*───([A-Za-z/"-]+):.*$/\1/' \
-	| sort \
-	| uniq)
+PACKAGES=$(nix flake show --json \
+	| jq '.packages."x86_64-linux" | keys[]')
 
 for package in $PACKAGES; do
 	nix build --verbose -L .#$package
