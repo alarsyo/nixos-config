ae2c988bdbb2b056c4210881f41f78b621307e2e
Antoine Martin <antoine@alarsyo.net>
2024-03-01 10:35:36 +0100

    talos: switch sddm to wayland

--- hosts/talos/default.nix
+++ hosts/talos/default.nix
@@ -100,7 +100,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
   services.xserver.desktopManager.plasma5.enable = true;
   services.power-profiles-daemon.enable = true;
 
