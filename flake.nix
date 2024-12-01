51e44c05d5383f18be7310e901ca2f0cd5bc6742
Antoine Martin <antoine@alarsyo.net>
2024-12-01 17:23:43 +0100

    flake: upgrade to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
       type = "github";
       owner = "NixOS";
       repo = "nixpkgs";
-      ref = "nixos-24.05";
+      ref = "nixos-24.11";
     };
 
     nixpkgs-unstable-small = {
@@ -25,7 +25,7 @@
       type = "github";
       owner = "nix-community";
       repo = "home-manager";
-      ref = "release-24.05";
+      ref = "release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
