b4d0d5f0a0964ea1f9354fa2191d5b7c405ae767
Antoine Martin <antoine@alarsyo.net>
2025-02-21 22:00:30 +0100

    home: tmux: disable escape time
    How am I only finding out about this now?

--- home/tmux.nix
+++ home/tmux.nix
@@ -19,6 +19,7 @@ in {
   config = mkIf cfg.enable {
     programs.tmux = {
       enable = true;
+      escapeTime = 0;
       baseIndex = 1;
       terminal = "screen-256color";
       clock24 = true;
